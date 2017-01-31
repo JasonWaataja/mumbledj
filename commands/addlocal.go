5c0d5b0623122e57c3cccec52cb6df30ce5bb1d5
Jason Waataja <jasonswaataja@gmail.com>
2017-01-31 12:24:07 -0800

    Wrote first version of addlocal command

--- commands/addlocal.go
+++ commands/addlocal.go
@@ -9,11 +9,13 @@ package commands
 
 import (
 	"errors"
+	"fmt"
 	"os"
 	"strings"
 
 	"github.com/layeh/gumble/gumble"
 	"github.com/matthieugrieger/mumbledj/bot"
+	"github.com/matthieugrieger/mumbledj/interfaces"
 	"github.com/matthieugrieger/mumbledj/services"
 	"github.com/spf13/viper"
 )
@@ -55,34 +57,71 @@ func (c *AddlocalCommand) Execute(user *gumble.User, args ...string) (string, bo
 
 	// If arguments were split around spaces, put them back together
 	// separated by spaces.
-	arg := strings.Join(args, " ")
+	localPath := strings.Join(args, " ")
 
-	path := bot.GetPathForLocalFile(arg)
-
-	if bot.PathIsSong(path) {
-		for _, service := range DJ.AvailableServices {
-			fs, ok := service.(services.Filesystem)
-			if ok {
-				track, err := fs.CreateTrackForLocalFile(path, user)
-				if err != nil {
-					return viper.GetString("commands.addlocal.track_creation_error", true, err)
-				} else {
-					return "", true, nil
-				}
-			}
-		}
-		return "", true, errors.New(viper.GetString("commands.addlocal.no_filesystem_service_error"))
-	} else if bot.PathIsPlaylist(path) {
-		for _, service := DJ.AvailableServices {
-			fs, ok := service.(services.Filesystem)
-			if ok {
-			}
-		}
-
-	}
+	path := bot.GetPathForLocalFile(localPath)
 
 	if _, err := os.Stat(path); err != nil {
 		return "", true, errors.New(viper.GetString("commands.addlocal.messages.no_matching_song_error"))
 	}
 
+	tracks := make([]interfaces.Track, 0)
+
+	if bot.PathIsSong(path) {
+		for _, service := range DJ.AvailableServices {
+			fs, ok := service.(*services.Filesystem)
+			if ok {
+				track, err := fs.CreateTrackForLocalFile(localPath, user)
+				if err != nil {
+					return "", true, err
+				} else {
+					tracks = append(tracks, track)
+				}
+			}
+		}
+		return "", true, errors.New(viper.GetString("commands.addlocal.messages.no_filesystem_service_error"))
+	} else if bot.PathIsPlaylist(path) {
+		for _, service := range DJ.AvailableServices {
+			fs, ok := service.(*services.Filesystem)
+			if ok {
+				newTracks, err := fs.CreateTracksForLocalFile(localPath, user)
+				if err != nil {
+					for _, track := range newTracks {
+						tracks = append(tracks, track)
+					}
+					return "", true, err
+				}
+			}
+		}
+	} else {
+		return "", true, errors.New(viper.GetString("commands.addlocal.messages.unrecognized_filetype_error"))
+	}
+
+	if len(tracks) == 0 {
+		return "", true, errors.New(viper.GetString("commands.addlocal.messages.no_valid_tracks_error"))
+	}
+
+	numTooLong := 0
+	numAdded := 0
+	var lastTrackAdded interfaces.Track
+	for _, track := range tracks {
+		if err := DJ.Queue.AppendTrack(track); err != nil {
+			numTooLong++
+		} else {
+			numAdded++
+			lastTrackAdded = track
+		}
+	}
+
+	if numAdded == 0 {
+		return "", true, errors.New(viper.GetString("commands.addlocal.messages.tracks_too_long_error"))
+	} else if numAdded == 1 {
+		return fmt.Sprintf(viper.GetString("commands.addlocal.messages.one_track_added"), user.Name, lastTrackAdded.GetTitle(), lastTrackAdded.GetService()), false, nil
+	}
+
+	retString := fmt.Sprintf(viper.GetString("commands.addlocal.messages.many_tracks_added"), user.Name, numAdded)
+	if numTooLong != 0 {
+		retString += fmt.Sprintf(viper.GetString("commands.addlocal.messages.num_tracks_too_long"), numTooLong)
+	}
+	return retString, false, nil
 }
