45cde559f779a46b69c9c733f548e101273c3257
Jason Waataja <jasonswaataja@gmail.com>
2017-01-31 07:47:33 -0800

    Continued writing it

--- services/filesystem.go
+++ services/filesystem.go
@@ -59,57 +59,11 @@ func (fs *Filesystem) CheckAPIKey() error {
 // if any error occurs during the API call.
 func (fs *Filesystem) GetTracks(url string, submitter *gumble.User) ([]interfaces.Track, error) {
 	tracks := make([]interfaces.Track, 0)
-	if fs.isTrack(url) {
-		path, err := bot.PathForFileURL(url)
-		if err != nil {
-			return tracks, err
-		}
-		track, err := fs.createTrackForFile(path, submitter)
-		if err == nil {
-			tracks = append(tracks, track)
-		} else {
-			return tracks, err
-		}
-	} else if fs.isPlaylist(url) {
-		path, err := bot.PathForFileURL(url)
-		if err != nil {
-			return tracks, err
-		}
-		reader, err := os.Open(path)
-		if err != nil {
-			return tracks, err
-		}
-		scanner := bufio.NewScanner(reader)
-		for scanner.Scan() {
-			line := scanner.Text()
-			if len(line) > 0 && !strings.HasPrefix(line, "#") {
-				track, err := fs.createTrackForFile(line, submitter)
-				if err != nil {
-					return tracks, err
-				}
-				tracks = append(tracks, track)
-			}
-		}
-	}
 	return tracks, nil
 }
 
-// createTrackForFile extracts the info from an mp3 file. Returns a tracks on
-// success and false on failure.
-func (fs *Filesystem) createTrackForFile(path string, submitter *gumble.User) (*bot.Track, error) {
-	mp3file, err := id3.Open(path)
-	defer mp3file.Close()
-	if err != nil {
-		return nil, err
-	}
-	basename := filepath.Base(path)
-	track := bot.Track{
-		ID:        basename,
-		URL:       "file://" + path,
-		Title:     mp3file.Artist(),
-		Submitter: submitter.Name,
-		Service:   fs.GetReadableName(),
-		Filename:  basename,
-	}
-	return &track, nil
+// CreateTrackForLocalFile creates a track that represents a file in the music
+// directory. Returns the track on success an nil on failure with an error.
+func CreateTrackForLocalFile(localPath string) (*bot.Track, error) {
+
 }
