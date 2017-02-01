04a17e841472b4cada27aa0de5a24b141fe7dd51
Jason Waataja <jasonswaataja@gmail.com>
2017-02-01 12:56:14 -0800

    Wrote utility function for listlocal
    Added two new structs that can scan files to create information about
    the songs contained inside them. Wrote method to turn that info into a
    string.

--- commands/listlocal.go
+++ commands/listlocal.go
@@ -8,7 +8,13 @@
 package commands
 
 import (
+	"errors"
+	"io/ioutil"
+	"os"
+	"time"
+
 	"github.com/layeh/gumble/gumble"
+	id3 "github.com/mikkyang/id3-go"
 	"github.com/spf13/viper"
 )
 
@@ -31,6 +37,94 @@ func (c *ListlocalCommand) Description() string {
 // returns false otherwise.
 func (c *ListlocalCommand) IsAdminCommand() bool {
 	return viper.GetBool("commands.listlocal.is_admin")
+}
+
+type MP3Info struct {
+	SongName string
+	Artist   string
+	Duration time.Duration
+}
+
+func NewMP3Info(path string) *MP3Info {
+	var songInfo MP3Info
+	reader, err := id3.Open(path)
+	if err != nil {
+		return nil
+	}
+	defer reader.Close()
+	songInfo.SongName = reader.Title()
+	songInfo.Artist = reader.Artist()
+	// TODO: Add duration.
+	return &songInfo
+}
+
+type SongDirectory struct {
+	Name    string
+	Entries []interface{}
+}
+
+func NewSongDirectory(name string) *SongDirectory {
+	var songDir SongDirectory
+	songDir.Name = name
+	return &songDir
+}
+
+func (songInfo *MP3Info) CreateInfo(indentation int) string {
+	var infoString string
+	for i := 0; i < indentation; i++ {
+		infoString += " "
+	}
+	infoString += songInfo.SongName + " " + songInfo.Artist
+	infoString += " (" + songInfo.Duration.String() + ")\n"
+
+	return infoString
+}
+
+func (songDir *SongDirectory) CreateInfo(indentation int) string {
+	var infoString string
+	for i := 0; i < indentation; i++ {
+		infoString += " "
+	}
+	infoString += songDir.Name + "\n"
+	for _, entry := range songDir.Entries {
+		switch t := entry.(type) {
+		case *MP3Info:
+			infoString += t.CreateInfo(indentation + 1)
+		case *SongDirectory:
+			infoString += t.CreateInfo(indentation + 1)
+		}
+	}
+	return infoString
+}
+
+func (songDir *SongDirectory) ScanDirectory(path string) error {
+	dirInfo, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
+	if !dirInfo.IsDir() {
+		// TODO: Replace this with a config message.
+		return errors.New("Attemping to scan non-directory for entries")
+	}
+	entries, err := ioutil.ReadDir(path)
+	if err != nil {
+		return err
+	}
+	for _, entry := range entries {
+		entryPath := path + "/" + entry.Name()
+		switch {
+		case entry.Mode().IsRegular():
+			songInfo := NewMP3Info(entryPath)
+			if songInfo != nil {
+				songDir.Entries = append(songDir.Entries, songInfo)
+			}
+		case entry.IsDir():
+			newDir := NewSongDirectory(entry.Name())
+			songDir.Entries = append(songDir.Entries, newDir)
+			go newDir.ScanDirectory(entryPath)
+		}
+	}
+	return nil
 }
 
 // Execute executes the command with the given user and arguments.
