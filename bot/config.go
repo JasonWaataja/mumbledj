1dd65b3b584d3f1d80ed855eddfe32a1e07d7df2
Jason Waataja <jasonswaataja@gmail.com>
2017-02-02 09:51:35 -0800

    Removed unneeded setting from various files

--- bot/config.go
+++ bot/config.go
@@ -143,7 +143,6 @@ func SetDefaultConfig() {
 	viper.SetDefault("commands.listlocal.is_admin", false)
 	viper.SetDefault("commadns.listlocal.description", "Lists available songs on the local filesystem.")
 	viper.SetDefault("commands.listlocal.messages.scan_non_directory_error", "Attemping to scan non-directory for entries")
-	viper.SetDefault("commands.listlocal.messages.no_argument_error", "No argument provided to listlocal")
 	viper.SetDefault("commands.listlocal.messages.scan_failure_error", "Failed to scan directory")
 
 	viper.SetDefault("commands.listtracks.aliases", []string{"listtracks", "listsongs", "list", "l"})
